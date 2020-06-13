3413e450eadbfba6ec98532f1e5bdcea15d13064
Samuel Pinillos <samuelpinillos@Samuels-MacBook-Air.local>
2020-06-12 22:26:00 -0500

    PARCIAL 3

--- archivo.php
+++ archivo.php
@@ -1,53 +1,100 @@
+<?php require("clases.php");?>
 <?php
+    $cadeConexion = baseDatos::conectarDB("csv");
+    foreach($Continentes as $continente){
+        baseDatos::GuardarBD2($continente-> ID,$continente-> Nombre,$cadeConexion);
+    }
+    session_start();
+
+    if(!isset($_SESSION["archivoCSV"])){
+        $_SESSION["archivoCSV"] = [];
+    }
+
+    if(isset($_SESSION["archivoCSV"])){
+        $archivoCSV = $_SESSION["archivoCSV"];
+    }
+
+    if(!isset($_SESSION["nombreArchivo"])){
+        $_SESSION["nombreArchivo"] = "";
+    }
+    $nombreArchivo =  $_SESSION["nombreArchivo"];
+    
     if(isset($_POST["submit"])){
-        $archivo = $_FILES['archivo'];
-
-        $nombreArchivo = $_FILES['archivo']['name'];
-        $directorioActual = $_FILES['archivo']['tmp_name'];
-
-        $separarNombre = explode('.', $nombreArchivo);
-        $extension = strtolower(end($separarNombre));
-        if ($extension == 'csv') {
-            $directorio = 'archivos/'.$nombreArchivo;
-            move_uploaded_file($directorioActual, $directorio);
-            $archivoValido = true;
-        } else {
-            echo "Archivo Invalido";
-            $archivoValido = false;
+        if($_FILES['archivo']['name'] != ""){
+            $archivo = $_FILES['archivo'];
+    
+            $nombreArchivo = $_FILES['archivo']['name'];
+            $directorioActual = $_FILES['archivo']['tmp_name'];
+            $_SESSION["nombreArchivo"] = $nombreArchivo;
+            $nombreArchivo =  $_SESSION["nombreArchivo"];
+            $separarNombre = explode('.', $nombreArchivo);
+    
+            $extension = strtolower(end($separarNombre));
+            if ($extension == 'csv') {
+                $directorio = 'archivos/'.$nombreArchivo;
+                move_uploaded_file($directorioActual, $directorio);
+                $archivoValido = true;
+                $archivoAbierto = fopen('archivos/'.$nombreArchivo, "r");
+                while ($contenido = fgetcsv($archivoAbierto)) {            
+                    if($contenido[0] != "id"){
+                        if (is_numeric($contenido[0]) && is_numeric($contenido[2]) && $contenido[2] < 6 && $contenido[2] > 0) {
+                            $nuevoPais = new Pais($contenido[0],$contenido[1],$contenido[2], true);
+                        } else {
+                            $nuevoPais = new Pais($contenido[0],$contenido[1],$contenido[2], false);   
+                        }
+                        Pais::agregaPais($archivoCSV,$nuevoPais);  
+                    }            
+                    $_SESSION["archivoCSV"] = $archivoCSV;
+                    $archivoCSV = $_SESSION["archivoCSV"];         
+                }
+                fclose($archivoAbierto);
+                unlink('archivos/'.$nombreArchivo);
+            } else {
+                echo "Archivo Invalido";
+                $archivoValido = false;
+            }
         }
+        else{
+            echo'No ha subido ningun archivo!';
+        }
+    }
+
+    if(isset($_POST["archivoNuevo"])){
+        session_destroy();
+        header("location: index.php");
     }
     
-    function leerCSV($nombre, $header=false) {
-        $archivoAbierto = fopen('archivos/'.$nombre, "r");
-        echo '<table class="table">';
-        echo '<thead>';
-        if ($header) {
-            $headerValidacion = ['id','nombre','total_departamentos'];
-            $contenido = fgetcsv($archivoAbierto);
-            if (Count($contenido) == 3) {
-                echo '<tr>';
-                    foreach ($contenido as $columnaHeader) {
-                        if (condition) {
-                            # code...
-                        }
-                        echo "<th>$columnaHeader</th>";
+    if(isset($_POST["SubirBasedatos"])){
+        $indice = 0;
+        $datosRepetidosBD = false;
+        if($archivoCSV != null){
+            foreach($archivoCSV as $pais){
+                if ($pais->FilaCorrecta == true) {
+                    $verifica = baseDatos::GuardarBD($pais-> ID, $pais-> Nombre,$pais->Continente,$cadeConexion);
+                    if($verifica){
+                        Pais::borraPais($archivoCSV,$indice);
+                        $_SESSION["archivoCSV"] = $archivoCSV;
+                        $archivoCSV = $_SESSION["archivoCSV"];
+                    }else{
+
+                        $datosRepetidosBD = true;
+                        
                     }
-                echo '</tr>';
+                } else {
+                    $indice++;
+                }    
             }
+
+            if ($indice > 0) {
+                echo'Hay datos incorectos!';echo'<br>';
+            }
+
+            if ($datosRepetidosBD == true){
+                echo'Hay datos repetidos <br>';
+            }                  
         }
-        echo '</thead>';
-            echo '<tbody>';
-            while ($contenido = fgetcsv($archivoAbierto)) {
-                echo '<tr>';
-                foreach ($contenido as $columna) {
-                    echo "<td>$columna</td>";
-                }
-                echo '</tr>';
-            }
-            echo '</tbody>';
-        echo '</table>';
-        fclose($archivoAbierto);
     }
+
 ?> 
 
 <!DOCTYPE html>
@@ -58,14 +105,37 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
+<body>  
     <div class="container">
+        <form method="POST">
+            <button class="btn btn-dark" name = "archivoNuevo">Subir Otro Archivo</button>
+            <button class="btn btn-dark" name = "SubirBasedatos">Subir a BD</button>
+
+        </form>
         <div class="table-responsive">
-            <?php
-                if ($archivoValido) {
-                    leerCSV($nombreArchivo, true);
-                }
-            ?>
+            <table class="table table-light table-bordered">
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Continente</th>
+                </tr>
+                <?php
+                    if($archivoCSV != null){
+                        foreach($archivoCSV as $dato){
+                            $nombreContinente = Continente::nombreContinente($dato-> Continente,$Continentes);
+
+                ?>
+                            <tr <?php if($dato-> FilaCorrecta == false){echo'class="bg-danger"';} ?>>
+                                <td><?php echo $dato-> ID; ?></td>
+                                <td><?php echo $dato-> Nombre; ?></td>
+                                <td><?php echo $nombreContinente; ?></td>
+                            </tr>
+                <?php
+                        }
+                    }
+                    //session_destroy();
+                ?>
+            </table>
         </div>
     </div>
 </body>
--- clases.php
+++ clases.php
@@ -1,14 +1,90 @@
 <?php
+
+    class baseDatos{
+        public static function conectarDB($baseDatos){
+            $host = 'localhost';
+            $user = 'root';
+            $contraseña = '';
+            $baseDatos = $baseDatos;
+
+            $conexionExitosa = mysqli_connect($host,$user,$contraseña);
+
+            mysqli_set_charset($conexionExitosa,'utf8');
+            mysqli_select_db($conexionExitosa,$baseDatos);
+
+            return $conexionExitosa;
+        }
+        public static function GuardarBD2($id, $nombre,$cadeConexion){
+            $sql = "INSERT INTO continentes (id,nombre) VALUES ('$id','$nombre')";
+            $verifica = mysqli_query($cadeConexion,$sql);
+
+            return $verifica;
+        }
+        public static function GuardarBD($id, $nombre,$continente,$cadeConexion){
+            $sql = "INSERT INTO paises (id,nombre,continente) VALUES ('$id','$nombre','$continente')";
+            $verifica = mysqli_query($cadeConexion,$sql);
+            
+            return $verifica;
+        }
+    }
+
+    class Continente{
+        public $ID;
+        public $Nombre;
+        
+        function __construct($ID = null, $Nombre = null)
+        {
+            if($ID){
+                $this-> ID = $ID;
+            }
+            if($Nombre){
+                $this-> Nombre = $Nombre;
+            }
+        }
+
+        public static function nombreContinente($id, $arrayContinentes){
+            foreach($arrayContinentes as $Continente){
+                if($Continente-> ID == $id){
+                    return $Continente-> Nombre;
+                }
+            }
+        } 
+    }
+
     class Pais{
         public $ID;
         public $Nombre;
-        public $TotalDepartamentos;
+        public $Continente;
 
+        public $FilaCorrecta;
 
-        function __Construct($paisID = null, $paisNOM = null, $totalDEP){
-            $this->ID = $paisID;
-            $this->Nombre = $paisNOM;
-            $this->TotalDepartamentos = $totalDEP;
+        function __Construct($ID = null, $Nombre = null, $Continente = null, $FilaCorrecta = null){
+            if($ID){
+                $this->ID = $ID;
+            }
+            if($Nombre){
+                $this->Nombre = $Nombre;
+            }
+            if($Continente){
+                $this->Continente = $Continente;
+            }
+            $this->FilaCorrecta = $FilaCorrecta;
+            
+        }
+        public static function agregaPais(&$arrayPaises, Pais $nuevoPais){
+            array_push($arrayPaises, $nuevoPais);
+        }
+        public static function borraPais(&$arrayPaises, $indice){
+            array_splice($arrayPaises, $indice, 1);
         }
     }
+
+    $Continentes = [
+        new Continente(1, "America"),
+        new Continente(2, "Asia"),
+        new Continente(3, "Africa"),
+        new Continente(4, "Oceania"),
+        new Continente(5, "Europa")
+    ];
+    
 ?>
